a88a97a9089a14ffbc6d2824a12536c473bc94a9
JeszC <null>
2025-05-25 16:35:07 +0300

    Add tests

--- test/scripts/utils.test.js
+++ test/scripts/utils.test.js
@@ -1,5 +1,5 @@
 import {describe, expect, it} from "vitest";
-import {compareNumeric} from "../../src/scripts/utils.js";
+import {compareNumeric, compareTextual} from "../../src/scripts/utils.js";
 
 describe("compareNumeric", () => {
     describe("given two (not NaN) numbers", () => {
@@ -31,6 +31,27 @@ describe("compareNumeric", () => {
             expect(compareNumeric(NaN, 5)).toBe(NaN);
             expect(compareNumeric(-3, NaN)).toBe(NaN);
             expect(compareNumeric(NaN, NaN)).toBe(NaN);
+        });
+    });
+});
+
+describe("compareTextual", () => {
+    describe("given two strings", () => {
+        it("it should return the difference between the two arguments", () => {
+            expect(compareTextual("test", "test")).toBe(0);
+            expect(compareTextual("test", "test ")).toBeLessThan(0);
+            expect(compareTextual("bca", "abc")).toBeGreaterThan(0);
+            expect(compareTextual("", "  ")).toBeLessThan(0);
+        });
+    });
+    describe("given two arguments of which either (or both) is not a string", () => {
+        it("should return 0", () => {
+            expect(compareTextual(null, null)).toBe(0);
+            expect(compareTextual(null, undefined)).toBe(0);
+            expect(compareTextual(undefined, undefined)).toBe(0);
+            expect(compareTextual([], {})).toBe(0);
+            expect(compareTextual([], [])).toBe(0);
+            expect(compareTextual(234, [])).toBe(0);
         });
     });
 });
