72822cee66bcc83321ff4a68c682ce25bb71d6dd
JeszC <null>
2025-06-24 22:56:20 +0300

    Refactor

--- src/components/shared/dialogs/game/components/GameContent.jsx
+++ src/components/shared/dialogs/game/components/GameContent.jsx
@@ -16,41 +16,39 @@ function GameContent({
                          onBack
                      }) {
 
-    return <>
-        <DialogContent fetchState={fetchState}
-                       closeDialog={closeDialog}
-                       onBack={onBack}
-                       headerData={<Header game={selectedGame}></Header>}
-                       bodyData={
-                           <>
-                               {
-                                   selectedGame && Object.keys(selectedGame).length > 0
-                                   ? <>
-                                       <GameInformation game={selectedGame}></GameInformation>
-                                       {
-                                           isGameUpcoming(selectedGame.gameState)
-                                           ? <UpcomingGames game={selectedGame}
-                                                            setPlayer={setPlayer}
-                                                            setActiveView={setActiveView}
-                                                            setPreviousView={setPreviousView}
-                                                            setFetchState={setFetchState}>
-                                           </UpcomingGames>
-                                           : <>
-                                               <PlayedGames game={selectedGame}
-                                                            setPlayer={setPlayer}
-                                                            setActiveView={setActiveView}
-                                                            setPreviousView={setPreviousView}
-                                                            setFetchState={setFetchState}>
-                                               </PlayedGames>
-                                           </>
-                                       }
-                                   </>
-                                   : null
-                               }
-                           </>
-                       }>
-        </DialogContent>
-    </>;
+    return <DialogContent fetchState={fetchState}
+                          closeDialog={closeDialog}
+                          onBack={onBack}
+                          headerData={<Header game={selectedGame}></Header>}
+                          bodyData={
+                              <>
+                                  {
+                                      selectedGame && Object.keys(selectedGame).length > 0
+                                      ? <>
+                                          <GameInformation game={selectedGame}></GameInformation>
+                                          {
+                                              isGameUpcoming(selectedGame.gameState)
+                                              ? <UpcomingGames game={selectedGame}
+                                                               setPlayer={setPlayer}
+                                                               setActiveView={setActiveView}
+                                                               setPreviousView={setPreviousView}
+                                                               setFetchState={setFetchState}>
+                                              </UpcomingGames>
+                                              : <>
+                                                  <PlayedGames game={selectedGame}
+                                                               setPlayer={setPlayer}
+                                                               setActiveView={setActiveView}
+                                                               setPreviousView={setPreviousView}
+                                                               setFetchState={setFetchState}>
+                                                  </PlayedGames>
+                                              </>
+                                          }
+                                      </>
+                                      : null
+                                  }
+                              </>
+                          }>
+    </DialogContent>;
 }
 
 export default GameContent;
--- src/components/shared/dialogs/player/components/PlayerContent.jsx
+++ src/components/shared/dialogs/player/components/PlayerContent.jsx
@@ -9,29 +9,27 @@ import Header from "./header/Header.jsx";
 
 function PlayerContent({selectedPlayer, fetchState, closeDialog, onBack}) {
 
-    return <>
-        <DialogContent fetchState={fetchState}
-                       closeDialog={closeDialog}
-                       onBack={onBack}
-                       headerData={<Header player={selectedPlayer}></Header>}
-                       bodyData={
-                           <>
-                               {
-                                   selectedPlayer && Object.keys(selectedPlayer).length > 0
-                                   ? <>
-                                       <PlayerInformation player={selectedPlayer}></PlayerInformation>
-                                       <CareerStatistics player={selectedPlayer}></CareerStatistics>
-                                       <Awards player={selectedPlayer}></Awards>
-                                       <Biography player={selectedPlayer}></Biography>
-                                       <LastFiveGames player={selectedPlayer}></LastFiveGames>
-                                       <SeasonHistory player={selectedPlayer}></SeasonHistory>
-                                   </>
-                                   : null
-                               }
-                           </>
-                       }>
-        </DialogContent>
-    </>;
+    return <DialogContent fetchState={fetchState}
+                          closeDialog={closeDialog}
+                          onBack={onBack}
+                          headerData={<Header player={selectedPlayer}></Header>}
+                          bodyData={
+                              <>
+                                  {
+                                      selectedPlayer && Object.keys(selectedPlayer).length > 0
+                                      ? <>
+                                          <PlayerInformation player={selectedPlayer}></PlayerInformation>
+                                          <CareerStatistics player={selectedPlayer}></CareerStatistics>
+                                          <Awards player={selectedPlayer}></Awards>
+                                          <Biography player={selectedPlayer}></Biography>
+                                          <LastFiveGames player={selectedPlayer}></LastFiveGames>
+                                          <SeasonHistory player={selectedPlayer}></SeasonHistory>
+                                      </>
+                                      : null
+                                  }
+                              </>
+                          }>
+    </DialogContent>;
 }
 
 export default PlayerContent;
