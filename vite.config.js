e693e54eee206b2982f2a805a9600f024de7e60a
JeszC <null>
2025-08-18 09:29:45 +0300

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import react from "@vitejs/plugin-react";
 import {defineConfig} from "vite";
 import eslint from "vite-plugin-eslint";
+import {VitePWA} from "vite-plugin-pwa";
 
 export default defineConfig({
     plugins: [
         react(),
-        eslint()
+        eslint(),
+        VitePWA(
+            {
+                registerType: "autoUpdate",
+                manifest: {
+                    name: "NHL Stats",
+                    short_name: "NHL Stats",
+                    icons: [
+                        {
+                            src: "images/favicon.svg",
+                            sizes: "any",
+                            type: "image/svg+xml",
+                            purpose: "any"
+                        }
+                    ]
+                }
+            }
+        )
     ],
     build: {
         sourcemap: false
